78c56209c839a96b54e116254c50d6a8a1c440dc
Charlie Whyman <65013284+charliewhyman@users.noreply.github.com>
2024-12-01 21:04:05 -0500

    Added initial header functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
+import { Header } from './components/Header';
 
 function App() {
 
   return (
       <Router>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-          </Routes>
+         <div className="min-h-screen bg-gray-50">
+         <Header/>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+            </Routes>
+          </div>
       </Router>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+export function Header() {
+
+    return (
+        <header className="bg-white shadow-sm">
+        <div className="mx-auto">
+            <div className="flex items-center justify-between ">
+            <a>DealHunt</a>
+            <a>Search deals</a>
+            <a>Submit a Deal</a>
+            <a>Login</a>
+            </div>
+        </div>
+        </header>
+    )
+}
