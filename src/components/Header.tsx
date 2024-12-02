e6b414b28810242783637098ad6e94fc15088178
Charlie Whyman <65013284+charliewhyman@users.noreply.github.com>
2024-12-01 21:07:58 -0500

    Added header home link and search bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,33 @@
-export function Header() {
+import { Link } from 'react-router-dom';
+import { Search, Tag } from 'lucide-react';
 
-    return (
-        <header className="bg-white shadow-sm">
-        <div className="mx-auto">
-            <div className="flex items-center justify-between ">
-            <a>DealHunt</a>
-            <a>Search deals</a>
-            <a>Submit a Deal</a>
-            <a>Login</a>
-            </div>
-        </div>
-        </header>
-    )
+export function Header() {
+  return (
+      <header className="bg-white shadow-sm">
+          <div className="mx-auto">
+              <div className="flex items-center justify-between">
+                  <div className="flex items-center">
+                      <Link to="/" className="flex items-center">
+                          <Tag className="" />
+                          <span className="font-bold">DealHunt</span>
+                      </Link>
+                      <div className="flex items-center">
+                          <div className="relative">
+                              <div className="absolute flex items-center ">
+                                  <Search className="text-gray-400" />
+                              </div>
+                              <input
+                                  type="text"
+                                  placeholder="Search deals..."
+                                  className="block w-full"
+                              />
+                          </div>
+                          <a>Submit a Deal</a>
+                          <a>Login</a>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </header>
+  )
 }
