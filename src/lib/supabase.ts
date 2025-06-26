3b389774eeb094b13cf1ff2d15a400a6bb9b4943
cwhyman <65013284+charliewhyman@users.noreply.github.com>
2025-06-26 11:49:12 +0100

    Removed redundant react-native import

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,12 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
-import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const supabaseUrl = 'https://arykyvofzxhxbfviccyo.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFyeWt5dm9menhoeGJmdmljY3lvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMwNjcxNjMsImV4cCI6MjA0ODY0MzE2M30.bQTiVAM84qzqyfL6FmEpzWauOLFpba8ZtK3DZSGj4rc';
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
     auth: {
-      storage: AsyncStorage,
       autoRefreshToken: true,
       persistSession: true,
       detectSessionInUrl: false,
