0780e8ccb03d953ec8b622f6cdb3f6b19c5384f0
65013284+charliewhyman@users.noreply.github.com <65013284+charliewhyman@users.noreply.github.com>
2025-12-15 14:28:39 +0000

    Cloudflare Pages compatibility comment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   
-  // 🔥Cloudflare Pages compatibility
+  // Cloudflare Pages compatibility
   define: {
     'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(process.env.VITE_SUPABASE_URL || ''),
     'import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY': JSON.stringify(process.env.VITE_SUPABASE_PUBLISHABLE_KEY || ''),
